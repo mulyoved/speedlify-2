8e427e28c237e43d9f5a56d5732b0be8978c4013
Muly Oved <mulyoved@gmail.com>
2020-07-05 11:45:20 +0300

    configure sites

--- _data/sites.js
+++ _data/sites.js
@@ -1,56 +1,14 @@
 module.exports = {
-	netlify: {
-		name: "Netlify", // optional, falls back to object key
-		description: "Netlify web sites",
+    testrtc: {
+		name: "testrtc", // optional, falls back to object key
+		description: "testrtc",
 		options: {
-			frequency: 60 * 24, // 24 hours
+			frequency: 60 * 1, // 24 hours
 			freshChrome: "site",
 		},
 		urls: [
-			"https://www.netlify.com/",
-			"https://www.netlify.com/donation-matching/",
-			"https://www.netlify.com/products/build/plugins/",
-			"https://www.netlify.com/with/sitecore/",
-			"https://www.netlify.com/with/drupal/",
-			"https://www.netlify.com/with/wordpress/",
-			"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
-			"https://jamstackconf.com/",
-			"https://jamstackconf.com/virtual/",
+            "https://staging-networktest.testrtc.com/",
+            "https://app.testrtc.com/",
 		]
 	},
-	ssg: {
-		name: "SSG", // optional, falls back to object key
-		description: "Static Site Generator web sites",
-		options: {
-			frequency: 60 * 24, // 24 hours
-			// if the sites don’t share assets on the same domain, we can reset
-			// chrome with each run instead of each site in every run (it’s faster)
-			freshChrome: "run"
-		},
-		urls: [
-			"https://www.11ty.dev/",
-			"https://www.gatsbyjs.org/",
-			"https://gohugo.io/",
-			"https://nextjs.org/",
-			"https://nuxtjs.org/",
-			"https://gridsome.org/",
-		]
-	},
-	zachleat: {
-		name: "zachleat.com", // optional, falls back to object key
-		description: "Personal web site",
-		options: {
-			frequency: 60 * 24, // 24 hours
-			freshChrome: "site"
-		},
-		urls: [
-			"https://www.zachleat.com/",
-			"https://www.zachleat.com/web/",
-			"https://www.zachleat.com/web/about/",
-			"https://www.zachleat.com/twitter/",
-			"https://www.zachleat.com/resume/",
-			"https://www.zachleat.com/web/comprehensive-webfonts/",
-			"https://www.zachleat.com/web/google-fonts-display/",
-		]
-	}
 };
